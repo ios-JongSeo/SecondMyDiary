c079058b4d916f1b190b075ecacc390ec20784c0
Kim Jong Seo <whdtj1450@naver.com>
2018-09-10 14:26:30 +0900

    폴더구조 변경

---
+++ SecondMyDiary/SecondMyDiary/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondMyDiary
+//
+//  Created by 김종서 on 2018. 9. 10..
+//  Copyright © 2018년 김종서. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
