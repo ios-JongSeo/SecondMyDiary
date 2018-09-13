87c633c24d8707a7166e7ae60acc9b449461eb62
Kim Jong Seo <whdtj1450@naver.com>
2018-09-13 15:17:50 +0900

    SecondMyDiary 테스트 작성 완료

--- SecondMyDiary/SecondMyDiaryTests/SecondMyDiaryTests.swift
+++ SecondMyDiary/SecondMyDiaryTests/SecondMyDiaryTests.swift
@@ -9,7 +9,15 @@
 import XCTest
 @testable import SecondMyDiary
 
+extension Entry {
+    static var dayBeforeYesterday: Entry { return Entry(id: 1, createdAt: Date.distantPast, text: "그저께 일기") }
+    static var yesterDay: Entry { return Entry(id: 2, createdAt: Date(), text: "어제 일기") }
+    static var today: Entry { return Entry(id: 3, createdAt: Date.distantFuture, text: "오늘 일기") }
+}
+
 class SecondMyDiaryTests: XCTestCase {
+    var newEntry: Entry!
+    
     func testEditEntryText() {
         // setup
         let entry = Entry(id: 0, createdAt: Date(), text: "첫 번째 일기")
@@ -39,11 +47,11 @@ class SecondMyDiaryTests: XCTestCase {
     
     func testGetEntryWithId() {
         // Setup
-        let oldEntry = Entry(id: 1, createdAt: Date(), text: "일기")
+        let oldEntry = Entry.yesterDay
         let journal = InMemoryDiary(entries: [oldEntry])
         
         // Run
-        let entry = journal.entry(with: 1)
+        let entry = journal.entry(with: oldEntry.id)
         
         // Verify
         XCTAssertEqual(entry, .some(oldEntry))
@@ -52,7 +60,7 @@ class SecondMyDiaryTests: XCTestCase {
     
     func testUpdateEntry() {
         // Setup
-        let oldEntry = Entry(id: 1, createdAt: Date(), text: "일기")
+        let oldEntry = Entry.yesterDay
         let journal = InMemoryDiary(entries: [oldEntry])
         
         // Run
@@ -60,7 +68,7 @@ class SecondMyDiaryTests: XCTestCase {
         journal.update(oldEntry)
         
         // Verify
-        let entry = journal.entry(with: 1)
+        let entry = journal.entry(with: oldEntry.id)
         XCTAssertEqual(entry, .some(oldEntry))
         XCTAssertTrue(entry?.isIdentical(to: oldEntry) == true)
         XCTAssertEqual(entry?.text, .some("일기가 수정 되었습니다."))
@@ -68,22 +76,22 @@ class SecondMyDiaryTests: XCTestCase {
     
     func testRemoveEntryFromJournal() {
         // Setup
-        let oldEntry = Entry(id: 1, createdAt: Date(), text: "일기")
+        let oldEntry = Entry.yesterDay
         let journal = InMemoryDiary(entries: [oldEntry])
         
         // Run
         journal.remove(oldEntry)
         
         // Verify
-        let entry = journal.entry(with: 1)
+        let entry = journal.entry(with: oldEntry.id)
         XCTAssertEqual(entry, nil)
     }
     
     func test_최근_순으로_엔트리를_불러올_수_있다() {
         // Setup
-        let dayBeforeYesterday = Entry(id: 1, createdAt: Date.distantPast, text: "그저께 일기")
-        let yesterday = Entry(id: 2, createdAt: Date(), text: "어제 일기")
-        let today = Entry(id: 3, createdAt: Date.distantFuture, text: "오늘 일기")
+        let dayBeforeYesterday = Entry.dayBeforeYesterday
+        let yesterday = Entry.yesterDay
+        let today = Entry.today
         
         let journal = InMemoryDiary(entries: [dayBeforeYesterday, yesterday, today])
         
@@ -97,9 +105,9 @@ class SecondMyDiaryTests: XCTestCase {
     
     func test_요청한_엔트리의_수만큼_최신_순으로_반환한다() {
         // Setup
-        let dayBeforeYesterday = Entry(id: 1, createdAt: Date.distantPast, text: "그저께 일기")
-        let yesterDay = Entry(id: 2, createdAt: Date(), text: "어제 일기")
-        let today = Entry(id: 3, createdAt: Date.distantFuture, text: "오늘 일기")
+        let dayBeforeYesterday = Entry.dayBeforeYesterday
+        let yesterDay = Entry.yesterDay
+        let today = Entry.today
         
         let journal = InMemoryDiary(entries: [dayBeforeYesterday, yesterDay, today])
         
@@ -113,9 +121,9 @@ class SecondMyDiaryTests: XCTestCase {
     
     func test_존재하는_엔트리보다_많은_수를_요청하면_존재하는_엔트리만큼만_반환한다() {
         // Setup
-        let dayBeforeYesterday = Entry(id: 1, createdAt: Date.distantPast, text: "그저께 일기")
-        let yesterDay = Entry(id: 2, createdAt: Date(), text: "어제 일기")
-        let today = Entry(id: 3, createdAt: Date.distantFuture, text: "오늘 일기")
+        let dayBeforeYesterday = Entry.dayBeforeYesterday
+        let yesterDay = Entry.yesterDay
+        let today = Entry.today
         
         let journal = InMemoryDiary(entries: [dayBeforeYesterday, yesterDay, today])
         
