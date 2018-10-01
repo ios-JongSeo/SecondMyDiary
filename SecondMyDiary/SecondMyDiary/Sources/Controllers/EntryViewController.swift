4e7e737f711ecc4bc361013fed97986a78cef795
Kim Jong Seo <whdtj1450@naver.com>
2018-10-01 16:38:54 +0900

    엔트리 조회 구현

--- SecondMyDiary/SecondMyDiary/Sources/Controllers/EntryViewController.swift
+++ SecondMyDiary/SecondMyDiary/Sources/Controllers/EntryViewController.swift
@@ -16,64 +16,6 @@ extension DateFormatter {
     }()
 }
 
-let code = """
-class EntryViewController: UIViewController {
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var button: UIButton!
-
-    let journal: secondMyDiary = InMemoryDiary()
-    private var editingEntry: Entry?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
-        textView.text = "TextView"
-
-        button.addTarget(self, action: #selector(saveEntry(_:)), for: .touchUpInside)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        textView.becomeFirstResponder()
-    }
-
-    @objc func saveEntry(_ sender: Any) {
-    if let editing = editingEntry {
-        editing.text = textView.text
-        journal.update(editing)
-    } else {
-        let entry: Entry = Entry(text: textView.text)
-        journal.add(entry)
-    }
-
-    updateSubviews(for: false)
-    }
-
-    @objc func editEntry(_ sender: Any) {
-        updateSubviews(for: true)
-    }
-
-
-    fileprivate func updateSubviews(for isEditing: Bool) {
-        if isEditing {
-            textView.isEditable = true
-            textView.becomeFirstResponder()
-
-            button.setTitle("저장", for: .normal)
-            button.addTarget(self, action: #selector(saveEntry(_:)), for: .touchUpInside)
-        } else {
-            textView.isEditable = false
-            textView.resignFirstResponder()
-
-            button.setTitle("수정", for: .normal)
-            button.addTarget(self, action: #selector(editEntry(_:)), for: .touchUpInside)
-        }
-    }
-}
-"""
-
 class EntryViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
@@ -85,12 +27,15 @@ class EntryViewController: UIViewController {
         return environment.entryRepository
     }
     
-    private var editingEntry: Entry?
+    var editingEntry: Entry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = DateFormatter.entryDateFormatter.string(from: Date())
-        textView.text = code
+        
+        let date: Date = editingEntry?.createdAt ?? Date()
+        
+        title = DateFormatter.entryDateFormatter.string(from: date)
+        textView.text = editingEntry?.text
         
         NotificationCenter.default
             .addObserver(self,
--- SecondMyDiary/SecondMyDiary/Sources/Controllers/TimelineViewController.swift
+++ SecondMyDiary/SecondMyDiary/Sources/Controllers/TimelineViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class TimelineViewController: UIViewController {
-    @IBOutlet weak var entryCountLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var environment: Environment!
+    private var entries: [Entry] = []
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let identifier = segue.identifier else { return }
@@ -19,6 +20,15 @@ class TimelineViewController: UIViewController {
         case "addEntry":
             let entryVC = segue.destination as? EntryViewController
             entryVC?.environment = environment
+            
+        case "showEntry":
+            if
+                let entryVC = segue.destination as? EntryViewController,
+                let selectedIndexPath = tableView.indexPathForSelectedRow {
+                entryVC.environment = environment
+                let entry = entries[selectedIndexPath.row]
+                entryVC.editingEntry = entry
+            }
         
         default:
             break
@@ -29,15 +39,30 @@ class TimelineViewController: UIViewController {
         super.viewDidLoad()
         
         title = "SecondMyDiary"
+        tableView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
        
-        let repo = environment.entryRepository
+        entries = environment.entryRepository.recentEntries(max: environment.entryRepository.numberOfEntries)
+        tableView.reloadData()
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return environment.entryRepository.numberOfEntries
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "EntryTableViewCell", for: indexPath)
         
-        entryCountLabel.text = repo.numberOfEntries > 0
-            ? "엔트리 갯수: \(repo.numberOfEntries)"
-            : "엔트리 없음"
+        let entry = entries[indexPath.row]
+        
+        tableViewCell.textLabel?.text = entry.text
+        tableViewCell.detailTextLabel?.text = DateFormatter.entryDateFormatter.string(from: entry.createdAt)
+        
+        return tableViewCell
     }
 }
